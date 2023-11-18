2693b5c6e76bc4bc661427b74e566be5432486c7
Jessica Sampaio <jessicamsampaio@gmail.com>
2023-11-18 11:40:18 -0300

    fix: limitacao de casas decimais

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat (prompt('Digite a nota mínima:'));
+const notaMinima = parseFloat (prompt("Digite a nota mínima:"));
 
 let linhas = '';
 
@@ -49,7 +49,7 @@ function atualizaTabela(){
 function atualizaMediaFinal () {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
